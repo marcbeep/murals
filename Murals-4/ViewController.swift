ecf0fa59ba0de140a2290d00b208438b4fc29075
marcbeep <77645923+marcbeep@users.noreply.github.com>
2022-12-12 15:59:12 +0000

    Initial set up of view controllers and loading API data

--- Murals-4/ViewController.swift
+++ Murals-4/ViewController.swift
@@ -7,11 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var murals:muralList? = nil
+    var info = ""
+    
+    func updateTheTable() {
+        theTable.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return murals?.newbrighton_murals.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "theCell", for: indexPath)
+            var content = UIListContentConfiguration.subtitleCell()
+            content.text = murals?.newbrighton_murals[indexPath.row].title ?? "Title Unavailable"
+            content.secondaryText = murals?.newbrighton_murals[indexPath.row].artist ?? "Artist Unavailable"
+            cell.contentConfiguration = content
+            return cell
+    }
+    
+    @IBOutlet weak var theTable: UITableView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+                     
+      if let url = URL(string: "https://cgi.csc.liv.ac.uk/~phil/Teaching/COMP228/nbm/data2.php?class=newbrighton_murals") {
+          let session = URLSession.shared
+            session.dataTask(with: url) { (data, response, err) in
+              guard let jsonData = data else {
+                  return
+              }
+              do {
+                  let decoder = JSONDecoder()
+                  let reportList = try decoder.decode(muralList.self, from: jsonData)
+                  self.murals = reportList
+                  DispatchQueue.main.async {
+                      self.updateTheTable()
+                  }
+              } catch let jsonErr {
+                  print("Error decoding JSON", jsonErr)
+              }
+          }.resume()
+       }
     }
 
 
---
+++ Murals-4/dataModel.swift
@@ -0,0 +1,29 @@
+//
+//  dataModel.swift
+//  Murals-4
+//
+//  Created by Marc Beepath on 12/12/2022.
+//
+
+import Foundation
+
+struct img: Decodable{
+    let id: String
+    let filename: String
+}
+
+struct murals: Decodable {
+    let id: String
+    let title: String
+    let artist: String?
+    let info: String?
+    let thumbnail: URL?
+    let lat: String?
+    let long: String?
+    let enabled: String
+    let lastModified: String
+    let images: [img]
+}
+struct muralList: Decodable {
+    let newbrighton_murals: [murals]
+}
